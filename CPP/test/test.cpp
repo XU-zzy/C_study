2dcbb50f5ed8dfc799e13c209efdd295df8001fb
XU-zzy <1245215743@qq.com>
2019-10-14 22:12:20 +0800

    C++ primer read

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int i = 1;
 int main()
 {
-    int i = 0;
-    int &r_1 = i;
-    const int &r_2 = i;
-    cout << i << r_1 << r_2 << endl;
-    
-    r_1 = 1;
-
-    cout << i << r_1 << r_2 << endl;
+    string str_1,str_t;
+    cin >> str_1;
+    for(auto c : str_1){
+        if(!ispunct(c)){
+            cout << c;
+        }
+    }
 
     return 0;
 }
-
