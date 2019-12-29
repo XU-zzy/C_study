19008ab81ed231c5ee82c623adf4b206575b23a3
XU-zzy <1245215743@qq.com>
2019-12-29 20:07:50 +0800

    huffman

--- CPP/test/test.cpp
+++ CPP/test/test.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
-vector<string> ph;
-int main()
-{
-    string n;
-    for(int i = 0;i < 10;i++){
-        cin >> n;
-        for(auto &c : n){
-            c = toupper(c);
-        }
-        ph.push_back(n);
-    }
-    
-    for(int i = 0;i < 10;i++)
-        cout << ph[i] << endl;
-    
-
+vector<int> num(100);
+int main(){
+    /* for(int i = 0;i < 100;i++) */
+        /* num.push_back(i); */
+    fill(num.begin(),num.end(),1);
+    int val;
+    cin >> val;
+    auto result = find(num.cbegin(),num.cend(),val);
+    cout << "The value " << val  << (result == num.cend() ? " is not present" : " is present" ) << endl;
+    cout << accumulate(num.cbegin(),num.cend(),50) << endl;
     return 0;
 }
