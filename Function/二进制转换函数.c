03cc9b85be7c773ed1491e8ebc6e54c537f3a01a
XU-zzy <1245215743@qq.com>
2020-02-14 19:15:25 +0800

    zz

--- Function/二进制转换函数.c
+++ Function/二进制转换函数.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-void bprint(int p){ 
-        int i=0;
-        int bit1 = 1;
-        for (i = sizeof (p) * 8 - 1;i >= 0; i--){
-                unsigned int x = ((( bit1 << i ) & p ) != 0);
-                printf("%d", x); 
-        }
 
-        printf("\n ");
+void bprint(int p) {
+    int i = 0;
+    int bit1 = 1;
+    for (i = sizeof(p) * 8 - 1; i >= 0; i--) {
+        unsigned int x = (((bit1 << i) & p) != 0);
+        printf("%d", x);
+    }
+
+    printf("\n ");
 }
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     bprint(n);
     return 0;
 }
