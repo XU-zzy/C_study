5ed3cd7b5ad5deff0633fb38d674ff95d73ecb32
XU-zzy <1245215743@qq.com>
2020-02-16 23:05:58 +0800

    zz

---
+++ CLionProjects/my_string/My_string.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zzy on 2020/2/16.
+//
+
+#include "My_string.h"
---
+++ CLionProjects/my_string/My_string.h
@@ -0,0 +1,14 @@
+//
+// Created by zzy on 2020/2/16.
+//
+
+#ifndef MY_STRING_H
+#define MY_STRING_H
+
+
+class My_string {
+
+};
+
+
+#endif //HUFFMAN_MY_STRING_H
