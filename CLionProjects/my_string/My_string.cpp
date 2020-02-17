8df40a6c40f708a7ffa37c10db9e3a3c0f3278a5
XU-zzy <1245215743@qq.com>
2020-02-17 23:01:11 +0800

    s

--- CLionProjects/my_string/My_string.cpp
+++ CLionProjects/my_string/My_string.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "My_string.h"
+
+My_string::My_string(int str_len) {
+    if(str_len < 0){
+        std::cout << "Wrong! str_len < 0!" << endl;
+        return -1;
+    }
+    this->str_len = str_len;
+    this->str = new char[this->str_len + 1];
+    memset(this->str,0,this->str_len + 1);
+}
+
+My_string::My_string(const char *str) {
+    if(*str == NULL){
+        this->str_len = 0;
+        this->str = new char[this->str_len + 1];
+    }
+}
--- CLionProjects/my_string/My_string.h
+++ CLionProjects/my_string/My_string.h
@@ -5,9 +5,21 @@
 #ifndef MY_STRING_H
 #define MY_STRING_H
 
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
 
 class My_string {
+public:
+    My_string(int str_len);
+    My_string(const char *str);
+    My_string(int str_len,char c);
+    ~My_string();
 
+private:
+    int str_len;
+    char *str;
 };
 
 
