cea28df44030e4c72b6d73d877ed8ff4b1c6fe25
XU-zzy <1245215743@qq.com>
2020-09-10 22:55:46 +0800

    z

---
+++ test/1.c
@@ -0,0 +1,49 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<linux/prctl.h>
+ 
+void descendant_process(void);
+ 
+int 
+main(void)
+{
+    prctl(PR_SET_CHILD_SUBREAPER,1);
+    //设置的child subreaper不会被继承
+    switch(fork()){
+        case 0:
+            descendant_process();
+            break;
+        case -1:
+            printf("Fork failed");
+            exit(-1);
+        default:
+            printf("Subreaper Process:%d\n",getpid());
+            sleep(10);
+            /* for(;;); */
+    }
+    return 0;
+}
+ 
+void
+descendant_process(void)
+{
+    switch(fork()){
+        case 0:
+            printf("Child process\n");
+            printf("child process: %d\n", getpid());
+            printf("father pid:%d\n", getppid());
+            sleep(5);
+            printf("now pid: %d\t ppid:%d \n",getpid(),getppid());
+            break;
+        case -1:
+            printf("Fork failed");
+            exit(-1);
+        default:
+            printf("Father Process\n");
+            sleep(1);
+            printf("Father exit\n");
+            break;
+    }    
+    return;
+}
---
+++ test/2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<linux/prctl.h>
+void fun(void){
+    switch(fork()){
+        case -1:
+            printf("Fork process 2 wrong!\n");
+        case 0:
+            printf("Child id: %d\n",getpid());
+            printf("Parent id: %d\n",getppid());
+            sleep(5);
+            printf("In the end,Child id: %d\t Parent id: %d\t\n",getpid(),getppid());
+            break;
+        default:
+            printf("Parent process\n");
+            sleep(1);
+            printf("Parent process exit\n");
+            break;
+    }
+    return;
+}
+int main(){
+    //prctl函数可以使当前调用它的进程变为subreaper进程
+    prctl(PR_SET_CHILD_SUBREAPER,1);
+    pid_t pid;
+    pid = fork();
+    switch(pid){
+        case -1:
+            printf("Fork process 1 wrong！\n");
+            exit(-1);
+        case 0:
+            fun();
+            break;
+        default:
+            printf("SUBREAPER id: %d\n",getpid());
+            sleep(10);
+            for(;;);
+    }
+    return 0;
+}
