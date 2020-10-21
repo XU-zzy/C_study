88d76e9587e022a674315a0b1ac3ad327ebf274a
XU-zzy <1245215743@qq.com>
2020-10-21 22:45:53 +0800

    del files

--- test/.helloworld.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/zzy/Study/test/helloworld.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/zzy/Study/test/helloworld.ko /home/zzy/Study/test/helloworld.o /home/zzy/Study/test/helloworld.mod.o ;  true
